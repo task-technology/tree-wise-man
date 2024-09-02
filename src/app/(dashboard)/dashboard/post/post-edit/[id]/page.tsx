34412c4e237fb3df2a0ea01223bb8cdf2ba38afc
Md Moniruzzaman <md.moniruzzaman1050@gmail.com>
2024-09-02 15:02:02 +0600

    merge with fahim

---
+++ src/app/(dashboard)/dashboard/post/post-edit/[id]/page.tsx
@@ -0,0 +1,11 @@
+import PostEdit from "@modules/Dashboard Content/Posts pages/Post Edit/PostEdit";
+
+const page = () => {
+  return (
+    <div>
+      <PostEdit />
+    </div>
+  );
+};
+
+export default page;
