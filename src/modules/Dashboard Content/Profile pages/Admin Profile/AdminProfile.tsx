50c177b4ba012ee87722372c6e7a851b624ebc9d
fahim-khandakar <fahimkhandakar01@gmail.com>
2024-09-22 16:19:02 +0600

    update admin  profile

--- src/modules/Dashboard Content/Profile pages/Admin Profile/AdminProfile.tsx
+++ src/modules/Dashboard Content/Profile pages/Admin Profile/AdminProfile.tsx
@@ -7,6 +7,7 @@ import { authKey } from "@config/constants";
 import { getUserInfo } from "../../../../shared/auth/auth.service";
 import LoadingSpinner from "@widgets/Loading Spinner/LoadingSpinner";
 import { useGetSingleAdminQuery } from "../../../../redux/features/api/admin";
+import { icons } from "@libs/Icons";
 
 const AdminProfile: FC<UserProfile> = () => {
   const token = getFromCookie(authKey);
@@ -28,29 +29,37 @@ const AdminProfile: FC<UserProfile> = () => {
           <div className="h-48 bg-blue-500">
             {/* You can use a background image here if desired */}
           </div>
-          <div className="absolute top-24 left-1/2 transform -translate-x-1/2">
-            <Image
-              src={singleData?.data?.profileImage}
-              alt={`${singleData?.data?.name}'s profile picture`}
-              width={150}
-              height={150}
-              priority
-              className="rounded-full  border-4 border-white"
-            />
-          </div>
+          {singleData?.data?.profileImage ? (
+            <div className="absolute top-24 left-1/2 transform -translate-x-1/2 text-center text-sm ">
+              <Image
+                src={singleData?.data?.profileImage}
+                alt={`${singleData?.data?.name}'s profile picture`}
+                width={150}
+                height={150}
+                priority
+                className="rounded-full  border-4 border-white"
+              />
+            </div>
+          ) : (
+            <span className=" text-4xl md:text-5xl p-8  md:p-12  absolute top-[122px] md:top-24 left-1/2 transform -translate-x-1/2 flex justify-center border border-grayForBorder rounded-full">
+              {icons?.user}
+            </span>
+          )}
         </div>
-        <div className="text-center mt-20 mb-8">
-          <h1 className="text-3xl font-bold text-gray-800">
+        <div className="text-center mt-10  md:mt-20 mb-8">
+          <h1 className="text-xl md:text-3xl font-bold text-gray-800">
             {singleData?.data?.name}
           </h1>
-          <p className="text-lg text-gray-600">{singleData?.data?.email}</p>
+          <p className="text-sm md:text-lg  text-gray-600">
+            {singleData?.data?.email}
+          </p>
         </div>
         <div className="px-6 py-4 border-t">
           <div className="flex justify-between items-center pb-2 md:pb-10">
-            <h2 className="text-xl font-semibold text-gray-800 mb-4">
+            <h2 className="text-lg md:text-xl font-semibold text-gray-800 mb-4">
               Details
             </h2>
-            <h2 className="text-sm font-semibold text-gray-800 mb-4">
+            <h2 className="text-xs md:text-sm font-semibold text-gray-800 mb-4">
               Joining Date:
               <span className="font-normal pl-1">
                 {singleData?.data?.createdAt?.slice(0, 10)}
@@ -59,16 +68,28 @@ const AdminProfile: FC<UserProfile> = () => {
           </div>
           <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
             <div className="bg-gray-50 p-4 rounded-lg shadow-md">
-              <h3 className="font-semibold text-gray-700">Phone Number</h3>
-              <p className="text-gray-600">{singleData?.data?.contactNo}</p>
+              <h3 className="text-sm md:text-base font-semibold text-gray-700">
+                Phone Number
+              </h3>
+              <p className="text-gray-600 text-xs md:text-base">
+                {singleData?.data?.contactNo}
+              </p>
             </div>
             <div className="bg-gray-50 p-4 rounded-lg shadow-md">
-              <h3 className="font-semibold text-gray-700">Company</h3>
-              <p className="text-gray-600">{singleData?.data?.company}</p>
+              <h3 className="text-sm md:text-base font-semibold text-gray-700">
+                Company
+              </h3>
+              <p className="text-gray-600 text-xs md:text-base">
+                {singleData?.data?.company}
+              </p>
             </div>
             <div className="bg-gray-50 p-4 rounded-lg shadow-md">
-              <h3 className="font-semibold text-gray-700">Designation</h3>
-              <p className="text-gray-600">{singleData?.data?.designation}</p>
+              <h3 className="text-sm md:text-base font-semibold text-gray-700">
+                Designation
+              </h3>
+              <p className="text-gray-600 text-xs md:text-base">
+                {singleData?.data?.designation}
+              </p>
             </div>
           </div>
         </div>
