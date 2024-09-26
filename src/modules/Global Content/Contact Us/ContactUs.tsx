cc8e68b7d0855abd7d1350a4a3f70949ceb1faad
fahim-khandakar <fahimkhandakar01@gmail.com>
2024-09-26 15:41:12 +0600

    upate contact us page's bg photo

--- src/modules/Global Content/Contact Us/ContactUs.tsx
+++ src/modules/Global Content/Contact Us/ContactUs.tsx
@@ -5,8 +5,7 @@ const ContactUs = () => {
     <div
       className="relative mt-20 min-h-screen bg-cover bg-center "
       style={{
-        backgroundImage:
-          'url("https://res.cloudinary.com/dvpnbsehd/image/upload/v1725425735/tree_n6tevw.png")',
+        backgroundImage: `url("/tree.png")`,
       }}
     >
       <div className="absolute inset-0 bg-black opacity-40"></div>
