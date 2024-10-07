ac8c85f26a3bec88ef31d8ca34cd8d4dcb133bd0
fahim-khandakar <fahimkhandakar01@gmail.com>
2024-10-07 15:08:22 +0600

    update pagination

--- src/common/widgets/Header/partials/user profile admin/UserProfileAdmin.tsx
+++ src/common/widgets/Header/partials/user profile admin/UserProfileAdmin.tsx
@@ -56,7 +56,7 @@ const UserProfileAdmin = () => {
           className=" menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
         >
           <li>
-            <Link href="/dashboard/profile/my-profile">
+            <Link href="/dashboard/profile/admin-profile">
               {singleData?.data?.name}
             </Link>
           </li>
